57f11425770116147a258afebeb153bca5260f05
Bianca A <frank@Bianca-Avila-4.local>
2017-11-09 20:32:16 +0900

    localhost running

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express'), 
+	mustacheExpress= require('mustache-express'),
+app = express(), 
+port = process.env.PORT || 8080 
+
+
+app.engine( 'html', mustacheExpress()); 
+app.set('view engine', 'html');
+app.set('views',__dirname + '/views');
+app.use(express.static(__dirname + 'public'));
+
+app.listen(port, ()=>{
+	console.log(`sever is listening on 🌚 🌝  ${port}`)
+});
+
+app.get('/', ( req, res)=>{
+	console.log("index rendering");
+	res.render('index')
+}); 
+
+
